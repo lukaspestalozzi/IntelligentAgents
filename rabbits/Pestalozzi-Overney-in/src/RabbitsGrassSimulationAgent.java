33038e20efaabbab1d36dd1538a2e42dafa2de9a
Lukas <lukas.pestalozzi@gmail.com>
2015-09-24 17:01:15 +0200

    changed the agents somewhat. They match the project description better now.

--- rabbits/Pestalozzi-Overney-in/src/RabbitsGrassSimulationAgent.java
+++ rabbits/Pestalozzi-Overney-in/src/RabbitsGrassSimulationAgent.java
@@ -3,62 +3,59 @@ import java.awt.Color;
 import uchicago.src.sim.gui.Drawable;
 import uchicago.src.sim.gui.SimGraphics;
 
-
 /**
-* Class that implements the simulation agent for the rabbits grass simulation.
-*
-* @author
-*/
+ * Class that implements the simulation agent for the rabbits grass simulation.
+ *
+ * @author
+ */
 
 public class RabbitsGrassSimulationAgent implements Drawable {
-  private int mX;
-  private int mY;
-  private int mEnergyLevel;
-  private int mStepsToLive;
-  private static int msIDNumber = 0;
-  private int mID;
+	private int mX;
+	private int mY;
+	private int mEnergyLevel;
+	private int mBirthTreshold;
+	private static int msIDNumber = 0;
+	private int mID;
 
-  public RabbitsGrassSimulationAgent(int minLifespan, int maxLifespan) {
-    mX = -1;
-    mY = -1;
-    mEnergyLevel = 0;
-    mStepsToLive =
-      (int)(Math.random() * (maxLifespan - minLifespan) + minLifespan);
-    mID = ++msIDNumber;
-  }
+	public RabbitsGrassSimulationAgent(int startEnergy, int birtThreshold) {
+		mX = -1;
+		mY = -1;
+		mEnergyLevel = startEnergy;
+		mBirthTreshold = birtThreshold;
+		mID = ++msIDNumber;
+	}
 
-  public void draw(SimGraphics target) {
-    target.drawFastRoundRect(Color.red);
-  }
+	public void draw(SimGraphics target) {
+		target.drawFastRoundRect(Color.red);
+	}
 
-  public int getX() {
-    return mX;
-  }
+	public int getX() {
+		return mX;
+	}
 
-  public int getY() {
-    return mY;
-  }
+	public int getY() {
+		return mY;
+	}
 
-  public void setXY(int newX, int newY) {
-    mX = newX;
-    mY = newY;
-  }
+	public void setXY(int newX, int newY) {
+		mX = newX;
+		mY = newY;
+	}
 
-  public String getID() {
-    return "A-" + mID;
-  }
+	public String getID() {
+		return "A-" + mID;
+	}
 
-  public int getEnergyLevel() {
-    return mEnergyLevel;
-  }
+	public int getEnergyLevel() {
+		return mEnergyLevel;
+	}
 
-  public int getStepsToLive() {
-    return mStepsToLive;
-  }
+	public int getBirthTreshold() {
+		return mBirthTreshold;
+	}
 
-  public void report() {
-    System.out.println(getID() + " at " + mX + ", " + mY + " has " +
-      getEnergyLevel() + " energy and " + getStepsToLive() + " steps to live.");
-  }
+	public void report() {
+		System.out.println(getID() + " at " + mX + ", " + mY + " has " + getEnergyLevel() + " energy.");
+	}
 
 }
--- rabbits/Pestalozzi-Overney-in/src/RabbitsGrassSimulationModel.java
+++ rabbits/Pestalozzi-Overney-in/src/RabbitsGrassSimulationModel.java
@@ -10,169 +10,163 @@ import uchicago.src.sim.gui.Object2DDisplay;
 import uchicago.src.sim.gui.Value2DDisplay;
 
 /**
-* Class that implements the simulation model for the rabbits grass
-* simulation.  This is the first class which needs to be setup in
-* order to run Repast simulation. It manages the entire RePast
-* environment and the simulation.
-*
-* @author 
-*/
+ * Class that implements the simulation model for the rabbits grass simulation.
+ * This is the first class which needs to be setup in order to run Repast
+ * simulation. It manages the entire RePast environment and the simulation.
+ *
+ * @author
+ */
 
+public class RabbitsGrassSimulationModel extends SimModelImpl {
 
-public class RabbitsGrassSimulationModel extends SimModelImpl {	
+	private static final int INIT_NUM_RABBITS = 10;
+	private static final int INIT_WOLRD_SIZE = 20;
+	private static final int TOTAL_GRASS_AMOUNT = 300;
+	private static final int BIRTH_THRESHOLD = 50;
+	private static final int INIT_ENERGY = 10;
 
-  private static final int INIT_NUM_AGENT = 10;
-  private static final int INIT_WOLRD_SIZE = 20;
-  private static final int TOTAL_GRASS_AMOUNT = 300;
-  private static final int AGENT_MIN_LIFESPAN = 30;
-  private static final int AGENT_MAX_LIFESPAN = 50;
+	private int mNumRabbits = INIT_NUM_RABBITS;
+	private int mWorldXSize = INIT_WOLRD_SIZE;
+	private int mWorldYSize = INIT_WOLRD_SIZE;
+	private int mGrassAmount = TOTAL_GRASS_AMOUNT;
+	private int mBirthThreshold = BIRTH_THRESHOLD;
+	private int mStartEnergy = INIT_ENERGY;
 
-  private int mNumAgents = INIT_NUM_AGENT;
-  private int mWorldXSize = INIT_WOLRD_SIZE;
-  private int mWorldYSize = INIT_WOLRD_SIZE;
-  private int mGrassAmount = TOTAL_GRASS_AMOUNT;
-  private int mAgentMinLifespan = AGENT_MIN_LIFESPAN;
-  private int mAgentMaxLifespan = AGENT_MAX_LIFESPAN;
+	private Schedule mSchedule;
+	private RabbitsGrassSimulationSpace mGrassFieldSpace;
+	private ArrayList<RabbitsGrassSimulationAgent> mAgentList;
+	private DisplaySurface mTiles;
 
-  private Schedule mSchedule;
-  private RabbitsGrassSimulationSpace mGrassFieldSpace;
-  private ArrayList<RabbitsGrassSimulationAgent> mAgentList;
-  private DisplaySurface mTiles;
+	public static void main(String[] args) {
+		SimInit init = new SimInit();
+		RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+		init.loadModel(model, "", false);
+	}
 
-  public static void main(String[] args) {
-    SimInit init = new SimInit();
-    RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
-    init.loadModel(model, "", false);
-  }
+	public void setup() {
+		System.out.println("[+] Running setup");
+		mGrassFieldSpace = null;
+		mAgentList = new ArrayList<RabbitsGrassSimulationAgent>();
 
-  public void setup() {
-    System.out.println("[+] Running setup");
-    mGrassFieldSpace = null;
-    mAgentList = new ArrayList<RabbitsGrassSimulationAgent>();
+		if (mTiles != null) {
+			mTiles.dispose();
+		}
+		mTiles = null;
 
-    if(mTiles != null) {
-      mTiles.dispose();
-    }
-    mTiles = null;
+		mTiles = new DisplaySurface(this, "Rabbit Grass Simulation Window 1");
+		registerDisplaySurface("Rabbit Grass Simulation Window 1", mTiles);
+	}
 
-    mTiles = new DisplaySurface(this, "Rabbit Grass Simulation Window 1");
-    registerDisplaySurface("Rabbit Grass Simulation Window 1", mTiles);
-  }
+	public void begin() {
+		buildModel();
+		buildSchedule();
+		buildDisplay();
 
-  public void begin() {
-    buildModel();
-    buildSchedule();
-    buildDisplay();
+		mTiles.display();
+	}
 
-    mTiles.display();
-  }
+	public String getName() {
+		return "Hey it's me, Little Rabbit";
+	}
 
-  public String getName() {
-    return "Hey it's me, Little Rabbit";
-  }
+	public Schedule getSchedule() {
+		return mSchedule;
+	}
 
-  public Schedule getSchedule() {
-    return mSchedule;
-  }
+	public void buildModel() {
+		System.out.println(" ├─ Building model");
+		mGrassFieldSpace = new RabbitsGrassSimulationSpace(mWorldXSize, mWorldYSize);
+		mGrassFieldSpace.spreadGrass(mGrassAmount);
 
-  public void buildModel() {
-    System.out.println(" ├─ Building model");
-    mGrassFieldSpace =
-      new RabbitsGrassSimulationSpace(mWorldXSize, mWorldYSize);
-    mGrassFieldSpace.spreadGrass(mGrassAmount);
+		for (int i = 0; i < mNumRabbits; i++) {
+			addNewAgent();
+		}
 
-    for (int i = 0; i < mNumAgents; i++) {
-      addNewAgent();
-    }
+		for (RabbitsGrassSimulationAgent rabbit : mAgentList) {
+			rabbit.report();
+		}
+	}
 
-    for (RabbitsGrassSimulationAgent rabbit : mAgentList) {
-      rabbit.report();
-    }
-  }
+	public void buildSchedule() {
+		System.out.println(" ├─ Building schedule");
+	}
 
-  public void buildSchedule() {
-    System.out.println(" ├─ Building schedule");
-  }
+	public void buildDisplay() {
+		System.out.println(" ├─ Building display");
 
-  public void buildDisplay() {
-    System.out.println(" ├─ Building display");
+		ColorMap colorMap = new ColorMap();
 
-    ColorMap colorMap = new ColorMap();
+		for (int i = 1; i < 16; i++) {
+			colorMap.mapColor(i, new Color(0, i * 8 + 127, 0));
+		}
+		colorMap.mapColor(0, Color.white);
 
-    for (int i = 1; i < 16; i++) {
-      colorMap.mapColor(i, new Color(0, i * 8 + 127, 0));
-    }
-    colorMap.mapColor(0, Color.white);
+		Value2DDisplay displayGrass = new Value2DDisplay(mGrassFieldSpace.getCurrentGrassField(), colorMap);
 
-    Value2DDisplay displayGrass =
-      new Value2DDisplay(mGrassFieldSpace.getCurrentGrassField(), colorMap);
+		Object2DDisplay displayAgents = new Object2DDisplay(mGrassFieldSpace.getCurrentGrassField());
+		displayAgents.setObjectList(mAgentList);
 
-    Object2DDisplay displayAgents =
-      new Object2DDisplay(mGrassFieldSpace.getCurrentGrassField());
-    displayAgents.setObjectList(mAgentList);
+		mTiles.addDisplayable(displayGrass, "Grass");
+		mTiles.addDisplayable(displayAgents, "Agents");
+	}
 
-    mTiles.addDisplayable(displayGrass, "Grass");
-    mTiles.addDisplayable(displayAgents, "Agents");
-  }
+	private void addNewAgent() {
+		RabbitsGrassSimulationAgent newAgent = new RabbitsGrassSimulationAgent(mStartEnergy, mBirthThreshold);
+		mAgentList.add(newAgent);
+		mGrassFieldSpace.addAgent(newAgent);
+	}
 
-  private void addNewAgent() {
-    RabbitsGrassSimulationAgent newAgent = 
-      new RabbitsGrassSimulationAgent(mAgentMinLifespan, mAgentMaxLifespan);
-    mAgentList.add(newAgent);
-    mGrassFieldSpace.addAgent(newAgent);
-  }
+	public String[] getInitParam() {
+		String[] initParams = { "NumAgents", "WorldXSize", "WorldYSize", "StartEnergy", "BirthThreshold",
+				"GrassAmount" };
+		return initParams;
+	}
 
-  public String[] getInitParam(){
-    String[] initParams = {"NumAgents", "WorldXSize", "WorldYSize",
-      "AgentMinLifespan", "AgentMaxLifespan", "GrassAmount"};
-    return initParams;
-  }
+	public int getNumAgents() {
+		return mNumRabbits;
+	}
 
-  public int getNumAgents() {
-    return mNumAgents;
-  }
+	public void setNumAgents(int na) {
+		mNumRabbits = na;
+	}
 
-  public void setNumAgents(int na) {
-    mNumAgents = na;
-  }
+	public int getWorldXSize() {
+		return mWorldXSize;
+	}
 
-  public int getWorldXSize() {
-    return mWorldXSize;
-  }
+	public void setWorldXSize(int xSize) {
+		mWorldXSize = xSize;
+	}
 
-  public void setWorldXSize(int xSize) {
-    mWorldXSize = xSize;
-  }
+	public int getWorldYSize() {
+		return mWorldYSize;
+	}
 
-  public int getWorldYSize() {
-    return mWorldYSize;
-  }
+	public void setWorldYSize(int ySize) {
+		mWorldYSize = ySize;
+	}
 
-  public void setWorldYSize(int ySize) {
-    mWorldYSize = ySize;
-  }
+	public int getGrassAmount() {
+		return mGrassAmount;
+	}
 
-  public int getGrassAmount() {
-    return mGrassAmount;
-  }
+	public void setGrassAmount(int grassAmount) {
+		mGrassAmount = grassAmount;
+	}
 
-  public void setGrassAmount(int grassAmount) {
-    mGrassAmount = grassAmount;
-  }
+	public int getBirthThreshold() {
+		return mBirthThreshold;
+	}
 
-  public int getAgentMinLifespan() {
-    return mAgentMinLifespan;
-  }
-
-  public void setAgentMinLifespan(int i) {
-    mAgentMinLifespan = i;
-  }
-
-  public int getAgentMaxLifespan() {
-    return mAgentMaxLifespan;
-  }
-
-  public void setAgentMaxLifespan(int i) {
-    mAgentMaxLifespan = i;
-  }
+	public void setBirthThreshold(int birthThreshold) {
+		this.mBirthThreshold = birthThreshold;
+	}
+	
+	public int getStartEnergy() {
+		return mStartEnergy;
+	}
+	
+	public void setStartEnergy(int startEnergy) {
+		this.mStartEnergy = startEnergy;
+	}
 }
