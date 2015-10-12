1b6671cdf773757ff4cf64377c9acacbec109cd5
Lukas <lukas.pestalozzi@gmail.com>
2015-10-12 17:36:49 +0200

    total-profit printout added

--- pickUp/reactive/src/template/ReactiveAgent.java
+++ pickUp/reactive/src/template/ReactiveAgent.java
@@ -8,36 +8,42 @@ import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.topology.Topology;
 
-
 public class ReactiveAgent implements ReactiveBehavior {
-
-//	private Random random;
-//	private double pPickup;
-	private ActionTable mActionTable;
-
-	@Override
-	public void setup(Topology topology, TaskDistribution td, Agent agent) {
-
-		// Reads the discount factor from the agents.xml file.
-		// If the property is not present it defaults to 0.95
-		Double gamma = agent.readProperty("discount-factor", Double.class,
-				0.95);
-		
-		mActionTable = new ActionTableBuilder(topology.cities(), td).generateActionTable(gamma);
-	}
-	@Override
-	public Action act(Vehicle vehicle, Task availableTask) {
-	  Action best = mActionTable.bestAction(vehicle.getCurrentCity(), availableTask);
-	  System.out.println(best.toLongString());
-	  return best;
-		
-
-//		if (availableTask == null || random.nextDouble() > pPickup) {
-//			City currentCity = vehicle.getCurrentCity();
-//			action = new Move(currentCity.randomNeighbor(random));
-//		} else {
-//			action = new Pickup(availableTask);
-//		}
-//		return action;
-	}
+  
+  private ActionTable mActionTable;
+  // this variable keeps reference of the Agent object
+  Agent agent;
+  // this variable counts how many actions have passed so far
+  int counterSteps = 0;
+  
+  @Override
+  public void setup(Topology topology, TaskDistribution td, Agent agent) {
+    this.agent = agent;
+    // Reads the discount factor from the agents.xml file.
+    // If the property is not present it defaults to 0.95
+    Double gamma = agent.readProperty("discount-factor", Double.class, 0.95);
+    
+    mActionTable = new ActionTableBuilder(topology.cities(), td)
+        .generateActionTable(gamma);
+  }
+  
+  @Override
+  public Action act(Vehicle vehicle, Task availableTask) {
+    // this output gives information about the "goodness" of your agent (higher
+    // values are preferred)
+    if ((counterSteps > 0) && (counterSteps % 100 == 0)) {
+      System.out.println("REACTIVE AGENT: ");
+      System.out.println("The total profit after " + counterSteps + " steps is "
+          + agent.getTotalProfit() + ".");
+      System.out.println("The profit per action after " + counterSteps + " steps is "
+          + ((double) agent.getTotalProfit() / counterSteps) + ".");
+      System.out.println();
+    }
+    counterSteps++;
+    
+    Action best = mActionTable.bestAction(vehicle.getCurrentCity(), availableTask);
+    // System.out.println(best.toLongString());
+    return best;
+    
+  }
 }
--- pickUp/reactive/src/template/ReactiveGreedy.java
+++ pickUp/reactive/src/template/ReactiveGreedy.java
@@ -14,16 +14,35 @@ import logist.topology.Topology;
 
 public class ReactiveGreedy implements ReactiveBehavior {
   Random r = new Random();
-
+  // this variable keeps reference of the Agent object
+  Agent agent;
+  // this variable counts how many actions have passed so far
+  int counterSteps = 0;
+  
   @Override
   public void setup(Topology topology, TaskDistribution distribution, Agent agent) {
-    // TODO Auto-generated method stub
     
+    this.agent = agent;
   }
-
+  
   @Override
   public Action act(Vehicle vehicle, Task availableTask) {
-    return availableTask == null ? new Move(vehicle.getCurrentCity().neighbors().get(r.nextInt(vehicle.getCurrentCity().neighbors().size()))) : new Pickup(availableTask);
+    // this output gives information about the "goodness" of your agent (higher
+    // values are preferred)
+    if ((counterSteps > 0) && (counterSteps % 100 == 0)) {
+      System.out.println("REACTIVE GREEDY: ");
+      System.out.println("The total profit after " + counterSteps + " steps is "
+          + agent.getTotalProfit() + ".");
+      System.out.println("The profit per action after " + counterSteps + " steps is "
+          + ((double) agent.getTotalProfit() / counterSteps) + ".");
+      System.out.println();
+    }
+    counterSteps++;
+    
+    return availableTask == null
+        ? new Move(vehicle.getCurrentCity().neighbors()
+            .get(r.nextInt(vehicle.getCurrentCity().neighbors().size())))
+        : new Pickup(availableTask);
   }
-
+  
 }
